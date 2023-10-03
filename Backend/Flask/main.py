bf84c95e2692e73bccdd4c2decf6a3d6c924f75d
crtejavardhanreddy <121811360+crtejavardhanreddy@users.noreply.github.com>
2023-10-03 10:11:59 +0530

    Push

--- Backend/Flask/main.py
+++ Backend/Flask/main.py
@@ -1,17 +1,52 @@
-from flask import Flask, redirect, url_for
+## Integrate HTML with Flask
+
+# GET and POST
+
+from flask import Flask, redirect, url_for, render_template, request
 
 # WSGI Applitaion
 app=Flask(__name__)
 
-# Build the URL dynamically
-@app.route('/eligible/<int:age>')
-def eligible(age):
-    return 'Eligible' if age>20 else 'Not Eligible'
+@app.route('/')
+def welcome():
+    return render_template('index.html')
 
-@app.route('/age/<int:age>')
-def Age(age):
-    criteria = 'eligible'
-    return redirect(url_for(criteria,age=age))
+# Build the URL dynamically
+# @app.route('/success/<int:score>')
+# def success(score):
+#     return 'The user is passed with '+ str(score)
+    
+@app.route('/fail/<int:score>')
+def fail(score):
+    res = ""
+    if score > 50:
+        res = "PASS"
+    else:
+        res = "FAIL"
+    return render_template('result.html',result = res, marks =score)
+
+
+# @app.route('/results/int:marks>')
+# def results(marks):
+#     result = ""
+#     if marks < 50:
+#         result = 'fail'
+#     else:
+#         result = "success"
+#     return redirect(result,score = marks)
+
+# Result checker HTML pscore
+@app.route('/submit',methods=['POST','GET'])
+def submit():
+    total_score = 0
+    if request.method=='POST':
+        science = float(request.form['science'])
+        maths = float(request.form["maths"])
+        datascience = float(request.form["datascience"])
+        c = float(request.form["c"])
+        total_score = (science + maths + datascience + c)/4
+    return redirect(url_for('fail',score = total_score))
+
 
 
 if __name__=='__main__':
